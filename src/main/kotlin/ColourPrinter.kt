b34b0041dae15bdc71ff4b1f1d41601ab14447c7
Rob <robmoore121@gmail.com>
2018-04-29 19:32:00 +0100

    updated algorithm to account for synchronicity of screen print jobs - cannot just send messages to a text 'object'

---
+++ src/main/kotlin/ColourPrinter.kt
@@ -0,0 +1,35 @@
+/*Created on 29/04/18. */
+class ColourPrinter {
+    fun printRed(s: String) {
+        val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;196m$s'").start()
+        val readLine = proc.inputStream.bufferedReader().readLine()
+        print(readLine)
+    }
+
+    fun printGreen(s: String) {
+        val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;028m$s'").start()
+        val readLine = proc.inputStream.bufferedReader().readLine()
+        print(readLine)
+    }
+
+    fun printWhite(s: String) {
+        val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;255m$s'").start()
+        val readLine = proc.inputStream.bufferedReader().readLine()
+        print(readLine)
+    }
+
+    fun printlnRed(s: String) {
+        printRed(s)
+        println()
+    }
+
+    fun printlnGreen(s: String) {
+        printGreen(s)
+        println()
+    }
+
+    fun printlnWhite(s: String) {
+        printWhite(s)
+        println()
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,40 +2,8 @@
 @file:JvmName("MainClass")
 
 fun main(args: Array<String>) {
-    printRed("red  ")
-    printGreen("green  ")
-    printlnWhite("white")
-}
-
-fun printRed(s: String) {
-    val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;196m$s'").start()
-    val readLine = proc.inputStream.bufferedReader().readLine()
-    print(readLine)
-}
-
-fun printGreen(s: String) {
-    val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;028m$s'").start()
-    val readLine = proc.inputStream.bufferedReader().readLine()
-    print(readLine)
-}
-
-fun printWhite(s: String) {
-    val proc = ProcessBuilder("/bin/bash", "-c", "printf '\\e[38;5;255m$s'").start()
-    val readLine = proc.inputStream.bufferedReader().readLine()
-    print(readLine)
-}
-
-fun printlnRed(s: String) {
-    printRed(s)
-    println()
-}
-
-fun printlnGreen(s: String) {
-    printGreen(s)
-    println()
-}
-
-fun printlnWhite(s: String) {
-    printWhite(s)
-    println()
+    val colourPrinter = ColourPrinter()
+    colourPrinter.printRed("red  ")
+    colourPrinter.printGreen("green  ")
+    colourPrinter.printlnWhite("white")
 }
