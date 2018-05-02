7fcd7fb0495378404fa20d03ce4823138c45258e
Rob <robmoore121@gmail.com>
2018-05-02 23:11:31 +0100

    added test for marking incorrectly

--- src/test/kotlin/MultipleChoiceQuestionTest.kt
+++ src/test/kotlin/MultipleChoiceQuestionTest.kt
@@ -1,14 +1,24 @@
 /*Created on 02/05/18. */
-import junit.framework.TestCase
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class MultipleChoiceQuestionTest {
     @Test
-    fun canMarkMultipleChoiceQuestion() {
+    fun canMarkMultipleChoiceQuestionCorrect() {
         val q = MultipleChoiceQuestion("m in monkey", "მ", Triple("ო", "გ", "წ"))
 
         val mark = q.markAnswer("მ")
 
-        TestCase.assertTrue(mark.correct)
+        assertTrue(mark.correct)
+    }
+
+    @Test
+    fun canMarkMultipleChoiceQuestionIncorrect() {
+        val q = MultipleChoiceQuestion("m in monkey", "მ", Triple("ო", "გ", "წ"))
+
+        val mark = q.markAnswer("ო")
+
+        assertFalse(mark.correct)
     }
 }
