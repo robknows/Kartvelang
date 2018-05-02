49949d9f3e503ad38f91e4ed96a1298358e150b3
Rob <robmoore121@gmail.com>
2018-05-02 16:32:31 +0100

    fixed ridiculous exec-dir difference between intellij and gradle causing file-loading tests to crash

--- src/test/kotlin/WordTest.kt
+++ src/test/kotlin/WordTest.kt
@@ -1,13 +1,24 @@
 /*Created on 01/05/18. */
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import java.nio.file.Paths
 
 class WordTest {
+    private fun fp(relativeToRootDir: String): String {
+        val projectPath = Paths.get("").toAbsolutePath().toString()
+        val fileRelativePath = if (projectPath.endsWith("modules")) {
+            "../../$relativeToRootDir"
+        } else {
+            relativeToRootDir
+        }
+        return Paths.get(projectPath, fileRelativePath).toString()
+    }
+
     @Test
     fun canLoadQuestionsFromFile() {
         val wl = WordLoader()
-        val filename = "../../src/test/resources/elevator_smalltalk_words.json"
-        val words: WordLoader.LoadedWords = wl.load(filename)
+        val path = fp("src/test/resources/elevator_smalltalk_words.json")
+        val words: WordLoader.LoadedWords = wl.load(path)
         val greetings = words.greetings
         val farewells = words.farewells
         val phrases = words.phrases
