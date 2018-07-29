628b43ca3ee8824c10807c4c595a78d7f9c21364
Rob <robmoore121@gmail.com>
2018-07-29 20:54:55 +0100

    to have (animate nouns) lesson layout

---
+++ src/main/kotlin/course/ToHaveInanimate.kt
@@ -0,0 +1,28 @@
+/*Created on 29/07/18. */
+package course
+
+/* To have (inanimate nouns) + possessive case + adjectives
+
+Learns how to say "მე მაქვს დიდი სახლი" + vocab + letters
+
+Vocab:
+
+Conjugation:
+
+მე მაქვს
+შენ გაქვს
+მას აქვს
+ჩვენ გვაქვს
+თქვენ გაქვთ
+მათ აქვთ
+
+დიდი
+პატარა
+ახალი
+ძველი
+სახლი
+ლოგინი
+მანქანა
+მაგიდა
+
+ */
