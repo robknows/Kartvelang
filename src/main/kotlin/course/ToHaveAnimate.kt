79a4caef5c5902c16e1cef649997b8ac7b725aed
Rob <robmoore121@gmail.com>
2018-07-30 08:36:05 +0100

    To have (animate nouns) + family lesson

---
+++ src/main/kotlin/course/ToHaveAnimate.kt
@@ -0,0 +1,32 @@
+/*Created on 29/07/18. */
+package course
+
+/* To have (animate nouns) + family members
+
+Learns how to say "მე მყავს დიდი ოჯახი" + vocab + letters
+
+Vocab:
+
+Conjugation:
+
+მე მყავს
+შენ გყავს
+მას ჰყავს
+ჩვენ გვყავს
+თქვენ გყავთ
+მათ ჰყავთ
+
+ოჯახი
+დიდი
+პატარა
+ახალი
+ძველი
+მამა
+დედა
+ძმა
+და
+შვილი
+ვაჟიშვილი
+ქალიშვილი
+
+ */
